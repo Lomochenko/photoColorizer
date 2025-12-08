30e2866d25c2ae9c35fc2a04b81256720161e346
ali naseri <99781841+Lomochenko@users.noreply.github.com>
2025-12-08 09:19:23 +0100

    Fix Tailwind CSS configuration

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,6 +6,10 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
   ],
 
+  css: [
+    '~/assets/css/main.css'
+  ],
+
   runtimeConfig: {
     public: {
       apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:8000'
@@ -17,5 +21,12 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
     }
+  },
+
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    viewer: true,
   }
 })
